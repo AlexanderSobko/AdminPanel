9f1bfadc70f6090efca5d0f492fbfc9a8fe2dd24
Alexander Sobko <Mr.AlexanderSobko@yandex.ru>
2022-06-30 20:52:07 +0300

    Disable cors settings

--- src/main/java/com/Mr/AlexanderSobko/admin_panel/configs/SecurityConfig.java
+++ src/main/java/com/Mr/AlexanderSobko/admin_panel/configs/SecurityConfig.java
@@ -17,8 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
