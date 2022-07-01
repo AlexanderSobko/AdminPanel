d774b6d807e5f67da8a5f49b9a257b9e7b97732b
Alexander Sobko <Mr.AlexanderSobko@yandex.ru>
2022-07-01 10:00:59 +0300

    Update cors settings

--- src/main/java/com/Mr/AlexanderSobko/admin_panel/AdminPanelApplication.java
+++ src/main/java/com/Mr/AlexanderSobko/admin_panel/AdminPanelApplication.java
@@ -18,7 +18,10 @@ public class AdminPanelApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/178.154.195.111:80/**").allowedOrigins("*");
+				registry.addMapping("/**")
+						.allowedOrigins("http://178.154.195.111:80")
+						.allowedMethods("*")
+						.maxAge(3600);
 			}
 		};
 	}
